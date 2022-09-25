07cd7cb2217f3216308b488fe2637d343a34d77a
drs317lakshami <devrajkumar317@gmail.com>
2022-09-25 14:41:07 +0530

    Total wage when queried by company

---
+++ ComputeEmpWage.java
@@ -0,0 +1,8 @@
+package com.blz.empwageoops;
+
+public interface ComputeEmpWage {
+	
+	public void addCompany(String companyName, int empRatePerHour, int numOfWorkingDays, int maxHoursInMonth);
+		public void computeEmpWage();
+		public int getTotalEmpWage(String company);
+	}
